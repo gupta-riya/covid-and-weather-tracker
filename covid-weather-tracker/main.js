292eb9a8cf523392acee0be52abbdcf86e09fd5a
gupta-riya <reeya211999@gmail.com>
2021-06-06 21:02:12 +0530

    tracker completed

--- covid-weather-tracker/main.js
+++ covid-weather-tracker/main.js
@@ -3,7 +3,11 @@ import { OrbitControls } from "https://unpkg.com/three@0.126.1/examples/jsm/cont
 
 
 let globeContainer = document.querySelector(".map-container");
-
+var covidIcon = document.querySelector(".covid-icon");
+var weatherIcon = document.querySelector(".weather-icon");
+let imgContainer = document.querySelector(".image-container");
+let displayInfoContainer = document.querySelector('.display-info-container');
+let infoContainer = document.querySelector('.info-container');
 
 //////////////////////////////data loading
 
@@ -151,7 +155,7 @@ controls.saveState();
 
 //----------- add event listeners ----------
 
-globeContainer.addEventListener("resize", onWindowResize, false);
+window.addEventListener("resize", onWindowResize, false);
 globeContainer.addEventListener("click", onWindowClick,false);
 globeContainer.addEventListener("mousemove", onWindowHover ,false);
 
@@ -180,7 +184,19 @@ function onWindowClick(event){
   
   for(let i = 0 ; i < intersects.length ; i++){
     
-   
+    var name = intersects[0].object.userData.country;
+    var confirm = numberWithCommas(intersects[0].object.userData.confirmed);
+    var dead = numberWithCommas(intersects[0].object.userData.dead);
+    var recover = numberWithCommas(intersects[0].object.userData.recovered);
+
+    if(!weatherIcon.classList.contains("active"))
+    {
+      document.querySelector(".info-title").innerText = name;
+      document.querySelector(".confirmed-cases").innerText = confirm;
+      document.querySelector(".death-cases").innerText = dead;
+      document.querySelector(".recovered-cases").innerText = recover;
+    }
+
     let divElem = document.createElement("div");
     divElem.setAttribute("class","country-info-container");
     divElem.style.position = "absolute";
@@ -190,13 +206,13 @@ function onWindowClick(event){
                 
     <div class="covid-cases-container">
         
-        <div class="card-title"><b>${intersects[0].object.userData.country}</b></div>
+        <div class="card-title"><b>${name}</b></div>
         <div class="card-spacer"></div>
         <hr />
         <div class="card-spacer"></div>
-        <div class = "cases-info">Cases: ${intersects[0].object.userData.confirmed}</div> 
-        <div class = "cases-info">Deaths: ${intersects[0].object.userData.dead}</div> 
-        <div class = "cases-info">Recovered: ${intersects[0].object.userData.recovered}</div>
+        <div class = "cases-info">Cases: ${confirm}</div> 
+        <div class = "cases-info">Deaths: ${dead}</div> 
+        <div class = "cases-info">Recovered: ${recover}</div>
         
         
     </div>`
@@ -241,9 +257,9 @@ function onWindowHover(event){
           <div class="card-spacer"></div>
           <hr />
           <div class="card-spacer"></div>
-          <div class = "cases-info">Cases: ${intersects[0].object.userData.confirmed}</div> 
-          <div class = "cases-info">Deaths: ${intersects[0].object.userData.dead}</div> 
-          <div class = "cases-info">Recovered: ${intersects[0].object.userData.recovered}</div>
+          <div class = "cases-info">Cases: ${numberWithCommas(intersects[0].object.userData.confirmed)}</div> 
+          <div class = "cases-info">Deaths: ${numberWithCommas(intersects[0].object.userData.dead)}</div> 
+          <div class = "cases-info">Recovered: ${numberWithCommas(intersects[0].object.userData.recovered)}</div>
           
           
       </div>`
@@ -365,3 +381,243 @@ function changeToCountry(data) {
 }
 
 // function to convert data into comma
+function numberWithCommas(x) {
+  return x.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+}
+
+
+
+// event listener for menu buttons
+
+covidIcon.addEventListener("click",function(){
+
+  if(!covidIcon.classList.contains("active"))
+  {
+    weatherIcon.classList.remove("active");
+    covidIcon.classList.add("active");
+    displayInfoContainer.remove();
+    infoContainer.innerHTML += ` <div class="image-container">
+    <img class = "covid-img" src="img/corona-virus.png" alt="covid-19">
+  </div>
+
+
+  <!-- ----------------------- display info ------------------- -->
+  <div class="display-info-container">
+
+      <div class="info-title"></div>
+      <div class="row">
+      <div class="card confirmed-card">
+        <span class="case-title">Confirmed Cases</span>
+        <img class="img" src="img/confirmed.gif">
+        <div class="info confirmed-info"><span class="confirmed-cases"></span></div>
+       </div>
+       <div class="card death-card">
+        <span class="case-title">Death Cases</span>
+        <img class="img" src="img/dead.gif">
+        <div class="info death-info"><span class="death-cases"></span></div>
+        </div>
+      </div>
+      <div class="row">
+        <div class="card recovered-card">
+          <span class="case-title">Recovered Cases</span>
+        <img class="img" src="img/recovered.gif">
+        <div class="info recovered-info"><span class="recovered-cases"></span></div>
+        </div>
+      </div>
+
+  </div>`;
+
+
+  }
+})
+
+weatherIcon.addEventListener("click",function(){
+
+  if(!weatherIcon.classList.contains("active"))
+  {
+    covidIcon.classList.remove("active");
+    weatherIcon.classList.add("active");
+    imgContainer.remove();
+    displayInfoContainer = document.querySelector('.display-info-container');
+    displayInfoContainer.innerHTML = "";
+    displayInfoContainer.classList.add('weather-display-info-container');
+    displayInfoContainer.innerHTML = ` <!-- ----------locations --------- -->
+          <div class="location-input-container">
+            <div class="current-location-container">
+            
+                <i class="fas fa-map-marker-alt">
+                  <span class="tooltiptext">Current Location</span>
+                </i>
+            </div>
+
+            <div class="container">
+              <input placeholder='Search for City' class='js-search' type="text">
+              <i class="fa fa-search"></i>
+            </div>
+          </div>
+
+          <!-- ------------ display details -->
+
+          <div class="display-data-container">
+
+            <div class="current-info-container">
+              <div class="day">Tuesday</div>
+              <div class="date">June 6,2021 11:05 PM</div>
+              <div class="city-name">Delhi</div>
+            </div>
+            <div class="current-temp-container">
+              <div class="temp-container">
+              
+                  <p class="temperature">
+                    <span class="temp">26</span><sup>°</sup>
+                  </p>
+
+              </div>
+              <div class="temp-img">
+                <i class="fas fa-cloud-sun-rain" aria-hidden="true"></i>
+              </div>
+            </div>
+          </div>
+
+          <!-- ------------- weather ------ -->
+          <div class="weather-type-container">
+            <div class="weather-type">Haze</div>
+            <div class="weather-status">It's Too Hot!!!</div>
+          </div>`;
+
+    let api_key = '742c39fd1acd4a416a031b2b2e4e2c91';
+
+
+    let currLoc = document.querySelector(".fa-map-marker-alt");
+    let inputCity = document.querySelector(".js-search");
+    let weekday = document.querySelector(".day");
+    let dateTime = document.querySelector(".date");
+    let cityName= document.querySelector(".city-name");
+    let temp = document.querySelector(".temp");
+    let daysArr = ['Sunday','Monday','Tuesday','Wednesday','Thursday','Friday','Saturday'];
+    let monthsArr = ['January','February','March','April','May','June','July','August','September','October','November','December'];
+    let mapContainer = document.querySelector('.weather-display-info-container');
+    let weatherType = document.querySelector('.weather-type');
+    let weatherStatus = document.querySelector('.weather-status');
+    currLoc.addEventListener("click",geoFindMe);
+    
+    inputCity.addEventListener("keydown",function(e)
+    {
+        if(e.key=="Enter")
+        {
+            let city = inputCity.value;
+            if(city!="")
+            {
+                inputCity.value="";
+                callWeatherApi(city,null,null);
+            }
+    
+            
+        }
+    });
+    
+    
+    async function callWeatherApi(city,latitude,longitude)
+    {
+        var weather_url = "";
+        if(city==null)
+        {
+            weather_url = `http://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=${api_key}`;
+        }
+        else
+        {
+            weather_url = `http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${api_key}`;
+        }
+        var fetchedData = await fetch(weather_url);
+        var data = await fetchedData.json();
+        await showWeather(data); 
+        
+    }
+    
+    async function showWeather(data)
+    {
+        
+        var weather = data.weather[0].main;
+        console.log(weather);
+        var date = new Date();
+        var hours = date.getHours();
+        var minutes = date.getMinutes();
+        
+        // Check whether AM or PM
+        var newformat = hours >= 12 ? 'PM' : 'AM'; 
+        
+        // Find current hour in AM-PM Format
+        hours = hours % 12; 
+        
+        // To display "0" as "12"
+        hours = hours ? hours : 12; 
+        minutes = minutes < 10 ? '0' + minutes : minutes;
+        
+        weekday.innerText = daysArr[date.getDay()];
+        dateTime.innerText = monthsArr[date.getMonth()] + " " + date.getDate() + "," + date.getFullYear() + " " + hours + ':' + minutes + ' ' + newformat;
+        cityName.innerText = data.name;
+        temp.innerText = getTemp(data.main.temp);
+    
+        weatherType.innerText = weather;
+        if(weather=='Rain')
+        {
+            mapContainer.style.backgroundImage = `url("https://www.google.com/url?sa=i&url=https%3A%2F%2Fforums.synfig.org%2Ft%2Fthere-must-be-a-better-way-to-do-this-animating-rain%2F3838&psig=AOvVaw24Eck2rsYkUcwLj7Zt4cC-&ust=1623074482821000&source=images&cd=vfe&ved=0CAIQjRxqFwoTCNDEteiVg_ECFQAAAAAdAAAAABAKf")`;
+            weatherStatus.innerText = "Don't forget your umbrellas!!!"
+        }
+        else if(weather=='Clouds')
+        {
+            mapContainer.style.backgroundImage = `url("https://i.pinimg.com/originals/e3/25/4b/e3254ba120e6e28688b59925ca5a277a.gif")`;
+            weatherStatus.innerText = "Head towards your home!!!"
+        }
+        else if(weather=='Clear')
+        {
+            mapContainer.style.backgroundImage = `url("https://i.graphicmama.com/blog/wp-content/uploads/2016/12/02102626/cartoon-landscape-vector.png")`;
+            weatherStatus.innerText = "All clear...You can plan a trip!!!";
+        }
+        else
+        {
+            mapContainer.style.backgroundImage = `url("https://cdn.dribbble.com/users/925716/screenshots/3333720/attachments/722376/after_noon.png?compress=1&resize=400x300")`;
+            weatherStatus.innerText = "It's hot today!!!"
+        }
+    
+    
+    
+    
+    }
+    
+    
+    
+    // delhi default call
+    callWeatherApi('delhi',null,null);
+
+  
+}
+})
+
+function getTemp(t)
+    {
+        return parseInt(t - 273);
+    }
+    
+function geoFindMe() {
+
+    function success(position) {
+      const latitude  = position.coords.latitude;
+      const longitude = position.coords.longitude;
+      callWeatherApi(null,latitude,longitude);
+  
+      
+    }
+  
+    function error() {
+      alert('Unable to retrieve your location');
+    }
+  
+    if(!navigator.geolocation) {
+      
+    } else {
+      
+      navigator.geolocation.getCurrentPosition(success, error);
+    }
+  
+  }
